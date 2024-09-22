542699c6061c68a99b76308549a607eea2fbac03
Matthew Topping <matthewatopping@gmail.com>
2024-09-23 08:14:34 +1200

    Add code for GitHub repository module

---
+++ main.tf
@@ -0,0 +1,93 @@
+resource "github_repository" "repository" {
+  name                                    = var.name
+  description                             = var.description
+  visibility                              = var.visibility != null ? var.visibility : "private"
+  has_issues                              = var.has_issues
+  has_projects                            = var.has_projects
+  has_wiki                                = var.has_wiki
+  is_template                             = var.is_template
+  allow_merge_commit                      = var.allow_merge_commit
+  allow_squash_merge                      = var.allow_squash_merge
+  allow_rebase_merge                      = var.allow_rebase_merge
+  allow_auto_merge                        = var.allow_auto_merge
+  delete_branch_on_merge                  = var.delete_branch_on_merge
+  auto_init                               = var.auto_init
+  archive_on_destroy                      = var.archive_on_destroy
+  archived                                = var.archived
+  topics                                  = length(var.topics) > 0 ? var.topics : null
+  vulnerability_alerts                    = var.vulnerability_alerts
+  ignore_vulnerability_alerts_during_read = var.ignore_vulnerability_alerts_during_read
+
+
+  homepage_url                = var.homepage_url != null ? var.homepage_url : null
+  squash_merge_commit_title   = var.squash_merge_commit_title != null ? var.squash_merge_commit_title : null
+  squash_merge_commit_message = var.squash_merge_commit_message != null ? var.squash_merge_commit_message : null
+  merge_commit_title          = var.merge_commit_title != null ? var.merge_commit_title : null
+  merge_commit_message        = var.merge_commit_message != null ? var.merge_commit_message : null
+  gitignore_template          = var.gitignore_template != null ? var.gitignore_template : null
+  license_template            = var.license_template != null ? var.license_template : null
+
+  dynamic "template" {
+    for_each = var.template != null ? [var.template] : []
+    content {
+      owner      = template.value.owner
+      repository = template.value.repository
+    }
+  }
+
+  dynamic "pages" {
+    for_each = var.pages != null ? [var.pages] : []
+    content {
+      source {
+        branch = pages.value.source.branch
+        path   = pages.value.source.path
+      }
+      build_type = pages.value.build_type
+      cname      = pages.value.cname
+    }
+  }
+}
+
+# Configuration for GitHub Actions secrets and variables
+# Manages both plaintext and encrypted secrets
+# Creates Actions variables from provided map
+# Enables flexible management of Actions configuration
+
+locals {
+  plaintext_secrets = {
+    for name, value in coalesce(var.plaintext_secrets, {}) : name => { plaintext = value }
+  }
+  encrypted_secrets = {
+    for name, value in coalesce(var.encrypted_secrets, {}) : name => { encrypted = value }
+  }
+
+  secrets = merge(local.plaintext_secrets, local.encrypted_secrets)
+}
+
+data "github_actions_public_key" "this" {
+  repository = github_repository.repository.name
+  depends_on = [
+    github_repository.repository
+  ]
+}
+
+resource "github_actions_secret" "repository_secret" {
+  for_each        = local.secrets
+  repository      = github_repository.repository.name
+  secret_name     = each.key
+  plaintext_value = try(each.value.plaintext, null)
+  encrypted_value = try(each.value.encrypted, null)
+  depends_on = [
+    github_repository.repository
+  ]
+}
+
+resource "github_actions_variable" "action_variable" {
+  for_each      = coalesce(var.actions_variables, {})
+  repository    = github_repository.repository.name
+  variable_name = each.key
+  value         = each.value
+  depends_on = [
+    github_repository.repository
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,195 @@
+variable "name" {
+  description = "The name of the repository"
+  type        = string
+}
+
+variable "description" {
+  description = "A description of the repository"
+  type        = string
+  default     = null
+}
+
+variable "homepage_url" {
+  description = "URL of a page describing the project"
+  type        = string
+  default     = null
+}
+
+variable "visibility" {
+  description = "Can be 'public' or 'private'"
+  type        = string
+  default     = null
+}
+
+variable "has_issues" {
+  description = "Set to true to enable the GitHub Issues features on the repository"
+  type        = bool
+  default     = null
+}
+
+variable "has_projects" {
+  description = "Set to true to enable the GitHub Projects features on the repository"
+  type        = bool
+  default     = null
+}
+
+variable "has_wiki" {
+  description = "Set to true to enable the GitHub Wiki features on the repository"
+  type        = bool
+  default     = null
+}
+
+variable "is_template" {
+  description = "Set to true to tell GitHub that this is a template repository"
+  type        = bool
+  default     = null
+}
+
+variable "allow_merge_commit" {
+  description = "Set to false to disable merge commits on the repository"
+  type        = bool
+  default     = null
+}
+
+variable "allow_squash_merge" {
+  description = "Set to false to disable squash merges on the repository"
+  type        = bool
+  default     = null
+}
+
+variable "allow_rebase_merge" {
+  description = "Set to false to disable rebase merges on the repository"
+  type        = bool
+  default     = null
+}
+
+variable "allow_auto_merge" {
+  description = "Set to true to allow auto-merging pull requests on the repository"
+  type        = bool
+  default     = null
+}
+
+variable "squash_merge_commit_title" {
+  description = "Can be 'PR_TITLE' or 'COMMIT_OR_PR_TITLE'"
+  type        = string
+  default     = null
+}
+
+variable "squash_merge_commit_message" {
+  description = "Can be 'PR_BODY', 'COMMIT_MESSAGES', or 'BLANK'"
+  type        = string
+  default     = null
+}
+
+variable "merge_commit_title" {
+  description = "Can be 'PR_TITLE' or 'MERGE_MESSAGE'"
+  type        = string
+  default     = null
+}
+
+variable "merge_commit_message" {
+  description = "Can be 'PR_BODY', 'PR_TITLE', or 'BLANK'"
+  type        = string
+  default     = null
+}
+
+variable "delete_branch_on_merge" {
+  description = "Automatically delete head branch after a pull request is merged"
+  type        = bool
+  default     = null
+}
+
+variable "auto_init" {
+  description = "Set to true to produce an initial commit in the repository"
+  type        = bool
+  default     = null
+}
+
+variable "gitignore_template" {
+  description = "Use the name of the template without the extension"
+  type        = string
+  default     = null
+}
+
+variable "license_template" {
+  description = "Use the name of the template without the extension"
+  type        = string
+  default     = null
+}
+
+variable "archived" {
+  description = "Specifies if the repository should be archived"
+  type        = bool
+  default     = null
+}
+
+variable "archive_on_destroy" {
+  description = "Set to true to archive the repository instead of deleting on destroy"
+  type        = bool
+  default     = null
+}
+
+variable "topics" {
+  description = "The list of topics of the repository"
+  type        = list(string)
+  default     = []
+}
+
+variable "template" {
+  description = "Template repository to use"
+  type = object({
+    owner      = string
+    repository = string
+  })
+  default = null
+}
+
+variable "vulnerability_alerts" {
+  description = "Set to true to enable security alerts for vulnerable dependencies"
+  type        = bool
+  default     = null
+}
+
+variable "ignore_vulnerability_alerts_during_read" {
+  description = "Set to true to not show vulnerability alerts during read"
+  type        = bool
+  default     = null
+}
+
+variable "pages" {
+  description = "GitHub Pages configuration"
+  type = object({
+    source = object({
+      branch = string
+      path   = optional(string)
+    })
+    build_type = optional(string)
+    cname      = optional(string)
+  })
+  default = null
+}
+
+
+variable "plaintext_secrets" {
+  description = "(Optional) Configuring plaintext actions secrets."
+  type        = map(string)
+  default     = {}
+}
+
+variable "encrypted_secrets" {
+  description = "(Optional) Configuring encryptyed actions secrets."
+  type        = map(string)
+  default     = {}
+}
+
+variable "actions_variables" {
+  description = "(Optional) Configuring actions variables."
+  type        = map(string)
+  default     = {}
+}
+
+variable "has_downloads" {
+  description = "Set to true to enable the (deprecated) downloads features on the repository"
+  type        = bool
+  default     = null
+}
